95cae96ae9672d7d048eb1248059d45af07f4c96
xJekk <jekkproducer@gmail.com>
2023-06-30 10:42:58 +0200

    database not showing sensible data

--- database/database.php
+++ database/database.php
@@ -2,9 +2,6 @@
 
 require_once "../config.php";
 
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
-
 $server = $server_name;
 $user = $user_name;
 $password = $db_password;
@@ -26,14 +23,6 @@ class Database {
         $this->user = $user;
         $this->password = $password;
         $this->db = $db;
-        // $this->server = "localhost:3306";
-        // $this->user = "root";
-        // $this->password = "root_miranda";
-        // $this->db = "mirandahoteldb";
-        // $this->server = getenv("MYSQL_SERVER");
-        // $this->user = getenv("MYSQL_USER");
-        // $this->password = getenv("MYSQL_DB_PASS");
-        // $this->db = getenv("MYSQL_DB");
     }
 
     public function executeQuery($query) {
