d4ab8624ceea14446dceabdfcc9b9fe6aca96957
xJekk <jekkproducer@gmail.com>
2023-06-30 11:04:35 +0200

    test

--- controllers/index.php
+++ controllers/index.php
@@ -5,9 +5,8 @@ require('../room/room.php');
 
 use eftec\bladeone\BladeOne;
 
-$views = __DIR__ . '/../views';
-$cache = __DIR__ . '/../cache';
-
+$views = __DIR__ . '../views';
+$cache = __DIR__ . '../cache';
 
 $blade = new BladeOne($views, $cache, BladeOne::MODE_AUTO);
 
